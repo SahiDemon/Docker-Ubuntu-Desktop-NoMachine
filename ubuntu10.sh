22716ccd8b650c7ce46e20b4cea9844b446dd880
Sahindu Gayanuka <65648007+SahiDemon@users.noreply.github.com>
2023-08-04 22:00:24 +0530

    Update ubuntu10.sh

--- ubuntu10.sh
+++ ubuntu10.sh
@@ -18,18 +18,7 @@ clear
 ./ngrok authtoken 2TWElHWvr5D8uIp1ZXRnH2o1aUE_5uRG4SE8hvzkRpoNnnLj4
 
 clear
-echo "======================="
-echo "choose ngrok region (for better connection)."
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 4000 &>/dev/null &
+./ngrok tcp --region ap 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
@@ -39,5 +28,4 @@ echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
 echo User: user
 echo Passwd: 123456
-
 
