48bd67b62493916a6cf22efca521b7d1b6c4fbd4
Sahindu Gayanuka <65648007+SahiDemon@users.noreply.github.com>
2023-08-04 22:24:49 +0530

    Update ubuntu10.sh

--- ubuntu10.sh
+++ ubuntu10.sh
@@ -28,4 +28,5 @@ echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
 echo User: user
 echo Passwd: 123456
+rm ubuntu10.sh
 exit
